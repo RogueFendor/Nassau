ffb06e218338038187c56d36cef990e82994c25a
Rogue <RogueFendorLabs@gmail.com>
2016-05-19 23:38:36 +0000

    added more dependancies

--- gui.sh
+++ gui.sh
@@ -27,11 +27,18 @@ apt-get install build-essential libtool autotools-dev autoconf pkg-configlibssl-
 apt-get install libboost-all-dev
 add-apt-repository ppa:bitcoin/bitcoin
 apt-get update
+apt-get install libboost­program­options­dev
 apt-get install libminiupnpc-dev
 apt-get install libdb4.8-dev
 apt-get install libdb4.8++-dev
 apt-get install libdb++-dev
 apt-get install libboost1.37-dev
+apt-get install libboost­thread­dev
+apt-get install libcurl­4­openssl­dev
+apt-get install libboost­filesystem­dev
+apt-get install qt­sdk
+apt-get install libminiupnpc­dev
+apt-get install openssl
 
 _main
 
